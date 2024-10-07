b4c55474d9c106c39321f46e81f53b70c21d5e4f
BaeSooMin <102912063+sumsum07@users.noreply.github.com>
2024-10-06 23:09:31 -0500

    checking duplication logic

--- src/main/java/capmap/org/capmapBE/service/UserService.java
+++ src/main/java/capmap/org/capmapBE/service/UserService.java
@@ -6,6 +6,7 @@ import capmap.org.capmapBE.dto.AddUserRequest;
 import capmap.org.capmapBE.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +18,10 @@ public class UserService {
     public Long save(AddUserRequest dto) {
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
+                .nickname(dto.getNickname())
+                .profile_pic(dto.getProfile_pic())
+                .user_level(dto.getUser_level())
+                .user_caps(dto.getUser_caps())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword()))
                 .build()).getId();
     }
@@ -24,6 +29,22 @@ public class UserService {
     public User findById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    /* 닉네임, 이메일 중복 여부 확인 */
+
+    @Transactional(readOnly = true)
+    //@Override
+    public boolean checkNicknameDuplication(String nickname) {
+        boolean nicknameDuplicate = userRepository.existsByNickname(nickname);
+        return nicknameDuplicate;
+
+    }
+    @Transactional(readOnly = true)
+    //@Override
+    public boolean checkEmailDuplication(String email) {
+        boolean emailDuplicate = userRepository.existsByEmail(email);
+        return emailDuplicate;
     }
 
 }
