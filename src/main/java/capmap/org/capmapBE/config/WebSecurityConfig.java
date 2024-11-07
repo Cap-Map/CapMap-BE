fa8e3d68e51abec35c922ca440333111bddd9b92
BaeSooMin <102912063+sumsum07@users.noreply.github.com>
2024-11-06 23:46:19 -0600

    api 이름 수정

--- src/main/java/capmap/org/capmapBE/config/WebSecurityConfig.java
+++ src/main/java/capmap/org/capmapBE/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
-                .requestMatchers("/hello", "/login", "/signup", "/user").permitAll()
+                .requestMatchers("/hello", "/login",  "/auth/user").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
